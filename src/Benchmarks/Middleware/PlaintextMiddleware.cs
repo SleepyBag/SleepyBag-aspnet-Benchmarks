e236779fdccbaf373cce1ecaba7e511eb24c2373
Xue Qianming <qianmingxue@microsoft.com>
2022-05-30 11:45:09 +0800

    Update lagging time

--- src/Benchmarks/Middleware/PlaintextMiddleware.cs
+++ src/Benchmarks/Middleware/PlaintextMiddleware.cs
@@ -25,7 +25,7 @@ namespace Benchmarks.Middleware
         public async Task Invoke(HttpContext httpContext)
         {
             Random waitTime = new Random();
-            int milliseconds = waitTime.Next(0, 500);
+            int milliseconds = waitTime.Next(0, 1000);
             await Task.Delay(milliseconds);
             if (httpContext.Request.Path.StartsWithSegments(_path, StringComparison.Ordinal))
             {
